3d3de9f4a87c74228c6ed54feed8ce09b52ab924
xiaoc57 <2415777318@qq.com>
2024-02-23 17:21:19 +0800

    add hw1 hw2 origin

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # this is mit s 980 课程， 逆向图形学
 
+# 环境配置
+
+    conda create -n s980 python=3.9
+    conda activate s980
+    !! pip torch requirements.txt
---
+++ homework/6S980-hw1/src/rendering.py
@@ -0,0 +1,16 @@
+from jaxtyping import Float
+from torch import Tensor
+
+
+def render_point_cloud(
+    vertices: Float[Tensor, "vertex 3"],
+    extrinsics: Float[Tensor, "batch 4 4"],
+    intrinsics: Float[Tensor, "batch 3 3"],
+    resolution: tuple[int, int] = (256, 256),
+) -> Float[Tensor, "batch height width"]:
+    """Create a white canvas with the specified resolution. Then, transform the points
+    into camera space, project them onto the image plane, and color the corresponding
+    pixels on the canvas black.
+    """
+
+    raise NotImplementedError("This is your homework.")
